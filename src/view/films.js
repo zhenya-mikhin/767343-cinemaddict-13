9f1556106c1dd1219671479b241cc5d97ef84060
zhenya <zhenek@119.1.192.168.lan.quantum.ru>
2020-12-02 20:06:07 +0300

    formatting functions

--- src/view/films.js
+++ src/view/films.js
@@ -1,9 +1,9 @@
 export const createFilmsTemplate = () => {
   return `<section class="films">
-  <section class="films-list">
-  <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-  <div class="films-list__container">
-  </div>
-  </section>
-</section>`;
+            <section class="films-list">
+              <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+              <div class="films-list__container">
+              </div>
+            </section>
+          </section>`;
 };
--- src/view/top-rated.js
+++ src/view/top-rated.js
@@ -1,7 +1,7 @@
 export const createTopRatedTemplate = () => {
   return `<section class="films-list films-list--extra">
-  <h2 class="films-list__title">Top rated</h2>
-  <div class="films-list__container">
-  </div>
-</section>`;
+            <h2 class="films-list__title">Top rated</h2>
+            <div class="films-list__container">
+            </div>
+          </section>`;
 };
