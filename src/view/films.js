1bfc69a7a1fe2172c11718399f6ab385ba4be5e6
zhenya-mikhin <zhenya.mikhin@bk.ru>
2021-01-22 19:36:37 +0300

    fix films.js

--- src/view/films.js
+++ src/view/films.js
@@ -2,23 +2,22 @@ import {createElement} from "../utils.js";
 
 const createFilmsTemplate = () => `<section class="films"></section>`;
 
-
 export default class FilmsView {
   constructor() {
     this._element = null;
   }
-  
+
   getTemplate() {
     return createFilmsTemplate();
   }
-  
+
   getElement() {
     if (!this._element) {
       this._element = createElement(this.getTemplate());
     }
     return this._element;
   }
-  
+
   removeElement() {
     this._element = null;
   }
